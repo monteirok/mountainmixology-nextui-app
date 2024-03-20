7c0bb38a031400bcbf85e0ae8bf50fc376d45fbe
monteirok <karstenmonteirodev@gmail.com>
2024-03-20 06:30:57 -0600

    created an REST API to fetch the menu

---
+++ app/api/[slug]/route.tsx
@@ -0,0 +1,20 @@
+export async function GET(request, {params}) {
+    const slug = params.slug
+    let response = {ok: false, message: 'Invalid Request'}
+
+    if (slug === 'menus') {
+        response = {
+            ok: true,
+            message: '',
+            menus: [
+                {"id":1,"title":"Xmas 2023 Menu","description":"A menu from a Xmas party in 2023.","category":"menus","thumbnail":"/images/menus/xmas-menu.png"},
+            ]
+        }
+    }
+
+    return Response.json(response)
+}
+
+export async function POST(request) {
+    const data = await request.json()
+}
--- app/menu/page.tsx
+++ app/menu/page.tsx
@@ -1,11 +1,37 @@
 import XmasMenu from '../components/Menus/XmasMenu';
 import Styles from './page.module.css';
 
-export default function Menu() {
-    return (
-      <main className={Styles.mainContainer}>
-        <XmasMenu />
-      </main>
-    );
+export default async function Menu() {
+  const getMenus = async () => {
+    const response = await fetch('http://localhost:3000/api/menus', {
+        next: {revalidate:10}
+    })
+    return response.json()
   }
+
+  const data = await getMenus()
+
+  return (
+    <>
+      <div className={Styles.dataMsg}>
+        {data?.message}
+      </div>
+      <main className={Styles.mainContainer}>
+        {/* <XmasMenu /> */}
+
+        {/* using custom api */}
+        {
+          data.menus.map((item, index) => (
+              <div className={Styles.menu} key={index}>
+                  <img src={item.thumbnail} alt={item.title}/>
+                  <div>
+                      {item.title}
+                  </div>
+              </div>
+          ))
+      }
+      </main>
+    </>
+  );
+}
   
